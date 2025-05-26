5a762d7749e299369ae0aa683c85c205fc54fb5e
fara <153857497+faraamalia@users.noreply.github.com>
2025-05-26 12:17:42 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,11 +8,11 @@ public class Driver {
 		
 		System.out.println("Count:" + counter.getCount());
 		
-		counter.increment();
+		counter.increment(10);
 		
 		System.out.println("Count:" + counter.getCount());
 		
-		counter.decrement();
+		counter.decrement(5);
 		
 		System.out.println("Count:" + counter.getCount());
 		
